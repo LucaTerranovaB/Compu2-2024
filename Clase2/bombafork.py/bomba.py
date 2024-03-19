d55712457d37468eae4c18e66eb78f14a4c301e6
Luca <l.terranova@alumno.um.edu.ar>
2024-03-19 15:51:09 -0300

    correccion 1

--- Clase2/bombafork.py/bomba.py
+++ Clase2/bombafork.py/bomba.py
@@ -3,47 +3,50 @@ import resource
 import time 
 import sys
 
-class BombaFork():
+
     
+    
+    
+def obtener_limites_recursos():
+    
+    
+    
+    soft,hard = resource.getrlimit(resource.RLIMIT_NPROC)
+    print("Límite suave:", soft)
+    time.sleep(5)
+    print("Límite duro:", hard)
+    time.sleep(5)
+    print("---------------------------------- \n")
+    
+    #establecemos un nuevo límite más bajo
+    resource.setrlimit(resource.RLIMIT_NPROC, (100, hard))
+    print("Límite de procesos hijo establecido en", 100)
+    time.sleep(5)
+        
+def bomba_fork():
     max_procesos = 100
-    
-    def obtener_limites_recursos(max_procesos):
-        soft,hard = resource.getrlimit(resource.RLIMIT_NPROC)
-        print("Límite suave:", soft)
-        time.sleep(5)
-        print("Límite duro:", hard)
-        time.sleep(5)
-        print("---------------------------------- \n")
-    
-        #establecemos un nuevo límite más bajo
-        resource.setrlimit(resource.RLIMIT_NPROC, (max_procesos, hard))
-        print("Límite de procesos hijo establecido en", max_procesos)
-        time.sleep(5)
         
-    def bomba_fork(max_procesos):
-        max_procesos = 100
-        
-        while max_procesos <= 100:
+    while max_procesos <= 100:
             
-            for i in range(max_procesos):
-                # Crear un nuevo proceso hijo
-                pid = os.fork()
-                time.sleep(2)
+        for i in range(max_procesos):
+            # Crear un nuevo proceso hijo
+            pid = os.fork()
+            time.sleep(2)
             
             
-            if pid == 0:
-                # Código ejecutado por el proceso hijo
-                time.sleep(2)
-                continue
-            else:
-                time.sleep(2)
-                print("Proceso hijo creado con PID:", pid)
-                print("---------------------------------- \n")
+        if pid == 0:
+            # Código ejecutado por el proceso hijo
+            time.sleep(2)
+            continue
+        else:
+            time.sleep(2)
+            print("Proceso hijo creado con PID:", pid)
+            print("---------------------------------- \n")
                 
 if __name__ == "__main__":
     # Llamar a la función para establecer el límite de procesos
-    BombaFork.obtener_limites_recursos(100)
+    obtener_limites_recursos(100)
     # Ejecutar la bromba fork
-    BombaFork.bomba_fork()    
+    bomba_fork(max_procesos=100)    
     
             
